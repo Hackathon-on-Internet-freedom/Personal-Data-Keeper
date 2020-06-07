96c1e90bcc2128b6fface808dc14b8ed63b072ca
Michael Pavlov <mr.michael.pavlov@gmail.com>
2020-06-07 15:23:06 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# @Author Michael Pavlov
+
+# PersonalDataKeeperBot
+TG_BOT_TOKEN = ''
+
+# proxy
+PROXY = {'http': 'socks5://127.0.0.1:9050', 'https': 'socks5://127.0.0.1:9050'}
+
+# admin account
+ADMIN_ID = ''
+
+# MySQL
+DB_DATABASE = ""
+DB_USER = ""
+DB_PASSWORD = ""
+DB_HOST = ""
+DB_PORT = ""
+
+# docs
+TMP_PATH = 'tmp/'
+PD_RECALL_TEMPLATE_FILE = 'pd_recall_template.docx'
+
+# timings
+PROCESSING_INTERVAL_SEC = 5
